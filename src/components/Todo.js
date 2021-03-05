ccfcaea782adbebec04d19cb0d5976aeefc9eea8
James Sinkler <jamessinkler713@gmail.com>
2021-03-05 15:23:19 -0800

    small fix on not using key as a prop

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Todo = (props) => {
     return (
-        <li data-todos-index={props.key}>
+        <li data-todos-index={props.todo._id}>
             <span className="todo-item">{props.todo.body}</span>
       </li> 
     );
