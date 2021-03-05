5e0d97d29c2906de697162c837388dfed6f76429
James Sinkler <jamessinkler713@gmail.com>
2021-03-05 15:11:18 -0800

    Sprint 4: Complete

--- src/containers/TodosContainer.js
+++ src/containers/TodosContainer.js
@@ -1,10 +1,12 @@
 import { useState, useEffect } from 'react'
 import TodoModel from '../models/Todo'
 import Todos from '../components/Todos'
+import CreateTodoForm from '../components/CreateTodoForm'
 
 export default function TodosContainer(){
     const [todos, setTodos] = useState([])
 
+    // put empty array to run once after initial render
     useEffect(()=> {
         const fetchData = async()=> {
             const res = await TodoModel.all()
@@ -13,10 +15,24 @@ export default function TodosContainer(){
         }
         fetchData()
     }, [])
-    
+
+    const createTodo = async(todo) => {
+        let newTodo = {
+            body: todo,
+            completed: false
+        }
+        const anotherTodo = await TodoModel.create(newTodo)
+        console.log(anotherTodo)
+        // taking our current todos, spreading and
+        // putting new one at the end
+        setTodos([...todos, anotherTodo.data])
+    }
+
+
 
     return (
         <div className='todosContainer'>
+            <CreateTodoForm createTodo={createTodo} />
             <Todos todos={todos} />
             <h1>TodosContainer</h1>
         </div>
--- src/models/Todo.js
+++ src/models/Todo.js
@@ -5,7 +5,6 @@ const endPoint = `https://sei-111-todo-backend.herokuapp.com/todos`;
 // const endPoint = 'localhost:8000/api/v1/todos'
 
 // router.get('/', (req, res)=> {
-
 //}
 
 // define a regular class
@@ -14,6 +13,11 @@ class TodoModel {
     static all = ()=> {
         let request = axios.get(endPoint)
         // this is going to take time it is a promise
+        return request
+    }
+    static create = (todo) => {
+        // post request sending the todo object as the second argument
+        let request = axios.post(endPoint, todo)
         return request
     }
     // methods to update delete etc.
