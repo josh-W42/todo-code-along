ff856b08306f4510429b9c05a1def8d81d97266b
James Sinkler <jamessinkler713@gmail.com>
2021-03-05 14:17:42 -0800

    Sprint 3: Complete

--- src/containers/TodosContainer.js
+++ src/containers/TodosContainer.js
@@ -1,8 +1,21 @@
 import { useState, useEffect } from 'react'
+import TodoModel from '../models/Todo'
+import Todos from '../components/Todos'
 
 export default function TodosContainer(){
+    const [todos, setTodos] = useState([])
+
+    useEffect(()=> {
+        const fetchData = async()=> {
+            const res = await TodoModel.all()
+            console.log(res)
+            setTodos(res.data)
+        }
+        fetchData()
+    })
     return (
         <div className='todosContainer'>
+            <Todos todos={todos} />
             <h1>TodosContainer</h1>
         </div>
     );
